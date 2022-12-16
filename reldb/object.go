7b8a64085ea42e8d4f590e9a2649723dd4537aad
Michel Paronnaud <michel.paronnaud@agileo-automation.com>
2022-12-16 17:34:55 +0100

    feat: implementation object

--- reldb/object.go
+++ reldb/object.go
@@ -1,6 +1,9 @@
 package reldb
 
-import "github.com/kataras/golog"
+import (
+	"github.com/kataras/golog"
+	"strings"
+)
 
 type IObject interface {
 	Hash() string
@@ -18,7 +21,7 @@ func NewObjWrapper(db IRelationalDB, ID string, value *IObject) *ObjWrapper {
 	return &ObjWrapper{db: db, ID: ID, Value: value}
 }
 
-func (t *ObjWrapper) Link(tableName string, ids ...string) {
+func (t *ObjWrapper) Link(biDirectional bool, tableName string, ids ...string) {
 	for _, id := range ids {
 		exist := t.db.Exist(tableName, id)
 		if !exist {
@@ -27,26 +30,37 @@ func (t *ObjWrapper) Link(tableName string, ids ...string) {
 		}
 		k := MakeLinkKey(t, tableName, id)
 		t.db.RawSet(PrefixLink, k[0], nil)
-		t.db.RawSet(PrefixLink, k[1], nil)
+		if biDirectional {
+			t.db.RawSet(PrefixLink, k[1], nil)
+		}
 	}
 }
 
-func (t *ObjWrapper) LinkNew(objs ...*IObject) []error {
-	return nil
-}
-
-func (t *ObjWrapper) DirectionalLink(tableName string, ids ...string) error {
-	return nil
-}
-
-func (t *ObjWrapper) DirectionalLinkNew(tableName string, objs ...*IObject) error {
-	return nil
+func (t *ObjWrapper) LinkNew(biDirectional bool, objs ...*IObject) {
+	for _, obj := range objs {
+		t.Link(biDirectional, (*t.Value).TableName(), t.db.Insert(obj).ID)
+	}
 }
 
 func (t *ObjWrapper) FromLink(tableName string, id string) *ObjWrapper {
+	K := MakeLinkKey(t, tableName, id)
+	rawGet, found := t.db.RawGet(PrefixLink, K[0])
+	if found {
+		return NewObjWrapper(t.db, id, Decode(rawGet))
+	}
 	return nil
 }
 
 func (t *ObjWrapper) AllFromLink(tableName string) []*ObjWrapper {
-	return nil
+	var objs []*ObjWrapper
+	t.db.RawIterKey(PrefixLink, func(key string) (stop bool) {
+		if strings.HasPrefix(key, (*t.Value).TableName()+Delimiter+t.ID+LinkDelimiter+tableName) {
+			objs = append(objs, t.db.Get(
+				tableName,
+				strings.Split(strings.Split(key, LinkDelimiter)[1], Delimiter)[1],
+			))
+		}
+		return false
+	})
+	return objs
 }
