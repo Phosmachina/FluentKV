17a40a6003f3458801f93631f1ece67da14d954f
Michel Paronnaud <michel.paronnaud@agileo-automation.com>
2023-03-01 17:51:10 +0100

    WIP feat: implement a collection toolkit

---
+++ tests/Collection_test.go
@@ -0,0 +1,142 @@
+package tests
+
+import (
+	. "git.antlia.tk/naxxar/FluentKV/reldb"
+	"testing"
+)
+
+func Test_NewCollection(t *testing.T) {
+	objs := []SimpleType{
+		NewSimpleType("val for t1", "val for t2", 1),
+		NewSimpleType("val for t1", "val for t2", 2),
+		NewSimpleType("val for t1", "val for t2", 3),
+	}
+	db := prepareTest()
+
+	for _, obj := range objs {
+		Insert(db, obj)
+	}
+
+	collection := NewCollection[SimpleType](db)
+	for i, objWrp := range collection.GetArray() {
+		if !objWrp.Value.Equals(objs[i]) {
+			t.Error("Array from collection are not equal to the original array.")
+			break
+		}
+	}
+}
+
+func Test_Sort(t *testing.T) {
+	objs := []SimpleType{
+		NewSimpleType("val for t1", "val for t2", 1),
+		NewSimpleType("val for t1", "val for t2", 2),
+		NewSimpleType("val for t1", "val for t2", 3),
+	}
+	db := prepareTest()
+
+	for _, obj := range objs {
+		Insert(db, obj)
+	}
+
+	collection := NewCollection[SimpleType](db)
+	collection.Sort(func(x, y *ObjWrapper[SimpleType]) bool {
+		return x.Value.Val > y.Value.Val
+	})
+	for i, objWrp := range collection.GetArray() {
+		if !objWrp.Value.Equals(objs[(len(objs)-1)-i]) {
+			t.Error("Objects are not properly ordered.")
+		}
+	}
+}
+
+func Test_Distinct(t *testing.T) {
+	objs := []SimpleType{
+		NewSimpleType("val for t1", "val for t2", 0),
+		NewSimpleType("duplicate1", "duplicate1", 1),
+		NewSimpleType("duplicate1", "duplicate1", 1),
+		NewSimpleType("duplicate2", "duplicate2", 2),
+		NewSimpleType("duplicate2", "duplicate2", 2),
+	}
+	db := prepareTest()
+
+	for _, obj := range objs {
+		Insert(db, obj)
+	}
+
+	collection := NewCollection[SimpleType](db)
+	collection.Distinct()
+
+	if collection.Len() != 3 {
+		t.Fatal("The collection has not the expected count of objects after Distinct.")
+	}
+	for i, objWrp := range collection.GetArray() {
+		if !objWrp.Value.Equals(objs[i*2]) {
+			t.Fatal("Not expected value, the arrays is corrupted or not in correct order after Distinct.")
+		}
+	}
+}
+
+func Test_Where(t *testing.T) {
+	objs := []SimpleType{
+		NewSimpleType("val for t1", "val for t2", 0),
+		NewSimpleType("val for t1", "val for t2", 1),
+		NewSimpleType("val for t1", "val for t2", 2),
+	}
+	objsTarget := []AnotherType{
+		NewAnotherType("", 10.3),
+		NewAnotherType("", 15),
+		NewAnotherType("", 7.12),
+	}
+	db := prepareTest()
+
+	for i, obj := range objs {
+		objWrp := Insert(db, obj)
+		LinkNew(objWrp, true, objsTarget[i])
+	}
+
+	collection := NewCollection[SimpleType](db)
+	Where[SimpleType, AnotherType](
+		true,
+		collection,
+		func(objWrp1 *ObjWrapper[SimpleType], objWrp2 *ObjWrapper[AnotherType]) bool {
+			return objWrp2.Value.Numeric > 10
+		},
+	)
+	if collection.Len() != 2 {
+		t.Fatal("The collection has not the expected count of objects after Where.")
+	}
+	for i, o := range collection.GetArray() {
+		if !o.Value.Equals(objs[i]) {
+			t.Fatal("Not expected value, " +
+				"the arrays is corrupted or not in correct order after Where.")
+		}
+	}
+}
+
+func Test_Filter(t *testing.T) {
+	objs := []SimpleType{
+		NewSimpleType("val for t1", "val for t2", 1),
+		NewSimpleType("val for t1", "val for t2", 2),
+		NewSimpleType("val for t1", "val for t2", 3),
+	}
+	db := prepareTest()
+
+	for _, obj := range objs {
+		Insert(db, obj)
+	}
+
+	collection := NewCollection[SimpleType](db)
+	collection.Filter(func(objWrp *ObjWrapper[SimpleType]) bool {
+		return objWrp.Value.Val <= 1
+	})
+	if len(collection.GetArray()) != 1 {
+		t.Fatal("The collection has not the expected amount of element after the filter.")
+	}
+	if !collection.GetArray()[0].Value.Equals(objs[0]) {
+		t.Fatal("The value of the collection is not what is expected.")
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO test complex cases based on SQL relevant situations (find them on sql.sh).
--- tests/IRelationalDB_test.go
+++ tests/IRelationalDB_test.go
@@ -1,63 +1,10 @@
 package tests
 
 import (
-	"encoding/gob"
-	. "git.antlia.tk/naxxar/FluentKV/impl"
 	. "git.antlia.tk/naxxar/FluentKV/reldb"
-	"os"
 	"strconv"
 	"testing"
 )
-
-//region DB Object type declaration
-
-type SimpleType struct {
-	DBObject
-	T1  string
-	T2  string
-	Val int
-}
-
-func NewSimpleType(t1 string, t2 string, val int) SimpleType {
-	simpleType := SimpleType{T1: t1, T2: t2, Val: val}
-	simpleType.IObject = simpleType
-	return simpleType
-}
-
-func (t SimpleType) ToString() string  { return ToString(t) }
-func (t SimpleType) TableName() string { return NameOfStruct[SimpleType]() }
-
-type AnotherType struct {
-	DBObject
-	T3      string
-	Numeric float32
-}
-
-func NewAnotherType(t3 string, numeric float32) AnotherType {
-	anotherType := AnotherType{T3: t3, Numeric: numeric}
-	anotherType.IObject = anotherType
-	return anotherType
-}
-
-func (t AnotherType) ToString() string  { return ToString(t) }
-func (t AnotherType) TableName() string { return NameOfStruct[AnotherType]() }
-
-//endregion
-
-// prepareTest clean previous data, register the DB type for Gob, create a new DB.
-func prepareTest() IRelationalDB {
-	// Clean previous db data
-	_ = os.RemoveAll("data")
-
-	// Register type used in db
-	gob.Register(SimpleType{})
-	gob.Register(AnotherType{})
-
-	// Initialize db
-	AutoKeyBuffer = 10
-	db, _ := NewBadgerDB("data")
-	return db
-}
 
 func Test_RawSetRawGet_SimpleType(t *testing.T) {
 	object := NewSimpleType("val for t1", "val for t2", 42)
