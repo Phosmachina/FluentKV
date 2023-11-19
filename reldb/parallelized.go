e83896eb6129a9181b4090c31a2c3b029bbf1cbc
Michel Paronnaud <paronnaud@disroot.org>
2023-11-19 17:46:41 +0100

    chore(parallelized): clean printing

--- reldb/parallelized.go
+++ reldb/parallelized.go
@@ -26,12 +26,10 @@ func NewTaskPool() *TaskPool {
 }
 
 func (tp *TaskPool) worker() {
-	//fmt.Println("[WORKER] Created...")
 
 	for {
 		select {
 		case task := <-tp.tasks:
-			//fmt.Println("[WORKER] Start a task.")
 			defer func() {
 				a := recover()
 				if a != nil {
@@ -40,10 +38,8 @@ func (tp *TaskPool) worker() {
 				}
 			}()
 			task()
-			//fmt.Println("[WORKER] Task is done.")
 			tp.wg.Done()
 		case <-tp.quit:
-			//fmt.Println("[WORKER] Receive quit signal.")
 			tp.mu.Lock()
 			tp.workers--
 			tp.mu.Unlock()
@@ -56,27 +52,24 @@ func (tp *TaskPool) AddTask(task Task) {
 	tp.mu.Lock()
 	if tp.workers < MaxTasks {
 		tp.workers++
-		//fmt.Println("[ADD] New worker created. Count:", tp.workers)
 		go tp.worker()
 	}
 	tp.mu.Unlock()
 	tp.wg.Add(1)
-	//fmt.Println("[ADD] Task is buffered.")
 	tp.tasks <- task
 }
 
 func (tp *TaskPool) Close() {
-	fmt.Println("[CLOSED] Wait all tasks are finished.")
+
 	tp.wg.Wait()
-	fmt.Println("[CLOSED] Send close to workers.")
+
 	tp.mu.Lock()
 	workers := tp.workers
 	tp.mu.Unlock()
+
 	for i := 0; i < workers; i++ {
-		//fmt.Println("[CLOSED] Send quit to worker:", i)
 		tp.quit <- true
 	}
-	fmt.Println("[CLOSED] Now close tasks channel.")
+
 	close(tp.tasks)
-	fmt.Println("[CLOSED] end.")
 }
