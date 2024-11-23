81ad1c38b62d51243eaaf297abd21196a187fe41
Michel Paronnaud <paronnaud@disroot.org>
2024-11-23 18:59:58 +0100

    refactor: move trigger struct in a separate file
    Add some dedicated errors for better handling

---
+++ core/trigger.go
@@ -0,0 +1,82 @@
+package core
+
+import "errors"
+
+var (
+	ErrCancelledByTrigger = errors.New("a trigger executed before cancelled the operation")
+	ErrDuplicateTrigger   = errors.New("the trigger with same Id for this table name is already added")
+	ErrInexistantTrigger  = errors.New("the trigger does not exist")
+)
+
+// Operation represents CRUD operations in bitmask format.
+type Operation int
+
+// Contain checks if the Operation in parameter is present in this Operation.
+func (o Operation) Contain(s Operation) bool {
+	return o&s == s
+}
+
+// CRUD Operation used for trigger as filter.
+const (
+	GetOperation Operation = 1 << iota
+	InsertOperation
+	DeleteOperation
+	UpdateOperation
+)
+
+type ITrigger interface {
+	GetId() string
+	GetTableName() string
+	GetOperation() Operation
+	IsBefore() bool
+	StartBefore(IKey, *IObject) bool
+	StartAfter(IKey, *IObject)
+	Equals(other ITrigger) bool
+}
+
+type trigger[T IObject] struct {
+	id         string
+	tableName  string
+	operations Operation
+	isBefore   bool
+	beforeTask func(key IKey, value *T) bool
+	afterTask  func(key IKey, value *T)
+}
+
+func (t trigger[T]) GetId() string {
+	return t.id
+}
+
+func (t trigger[T]) GetTableName() string {
+	return t.tableName
+}
+func (t trigger[T]) GetOperation() Operation {
+	return t.operations
+}
+
+func (t trigger[T]) IsBefore() bool {
+	return t.isBefore
+}
+
+func (t trigger[T]) StartBefore(key IKey, value *IObject) bool {
+	v := (*value).(T)
+	return t.beforeTask(key, &v)
+}
+
+func (t trigger[T]) StartAfter(key IKey, value *IObject) {
+	v := (*value).(T)
+	t.afterTask(key, &v)
+}
+
+func (t trigger[T]) Equals(other ITrigger) bool {
+	return t.GetTableName() == other.GetTableName() && t.GetId() == other.GetId()
+}
+
+func indexOf(t ITrigger, triggers []ITrigger) int {
+	for k, v := range triggers {
+		if t.Equals(v) {
+			return k
+		}
+	}
+	return -1 // not found.
+}
